891b5b7b9199355da0e25e87bd08cb97c1d20f00
JonathanDDavis <125523206+JonathanDDavis@users.noreply.github.com>
2024-05-15 21:48:18 -0500

    Add files via upload

---
+++ HelperMethods/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        private static void ReverseString(string message)
+        {
+            char[] messageArray = message.ToCharArray();
+            Array.Reverse(messageArray);
+            foreach (char item in messageArray)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+}
---
+++ HelperMethods/Root Beer Method/Root Beer Method/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Root_Beer_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many bottles of Root Beer do you want to take off the wall. Please put a number in that is above 0.");
+            int x = int.Parse(Console.ReadLine());
+            countRootBeer(x);
+            
+            Console.ReadLine();
+        }
+
+        private static void countRootBeer(int x)
+        {
+            int counter = -1;
+            while (counter < x)
+            {
+                Console.WriteLine("");
+                Console.Write("{0} bottles of root beer on the wall {0} bottles of root beer. Take one down and pass it around {1} bottles of root beer on the wall.", x, x - 1);
+                x--;
+            }
+        }
+    }
+}
